5f577b437f2ea7ddba600e1eca8daca432788158
Bianca Baias <biaax6@yahoo.com>
2024-10-10 19:43:37 +0300

    Deleted commented code and useless for

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import json
 
-# Based on my counting function, I estimated that the following are the most common letters, in descending order
-#vowels = ['i', 'e', 'a', 'o', 'u', 'ă', 'â' , 'î']
-#consonants = ['r', 'l', 't', 'n', 'c', 's', 'm', 'd', 'p', 'g', 'b', 'f', 'v', 'z', 'h', 'ș', 'ț', 'j', 'k', 'x', 'q', 'w', 'y']
 
 def read_file(filename):
     """
@@ -32,7 +29,6 @@ def check_vowels(word, vowels):
             trials += 1
             if vowel in word[2].lower():
                 indexes =  [i for i, letter in enumerate(word[2]) if letter.lower() == vowel]
-                #print(f"indexes for letter {vowel}: {indexes}")
                 for index in indexes:
                     guess_word[index] = vowel.upper()
             
@@ -62,7 +58,6 @@ def check_consonants(word, consonants):
             
             if cons in word[2].lower():
                 indexes =  [i for i, letter in enumerate(word[2]) if letter.lower() == cons]
-                #print(f"indexes for letter {cons}: {indexes}")
                 for index in indexes:
                     guess_word[index] = cons.upper()
             
@@ -105,19 +100,13 @@ def counting(words):
     alphabet = "aăâbcdefghiîjklmnopqrsștțuvwxyz"
     result = {'a': 0, 'ă':0, 'â': 0, 'î': 0, 'ș': 0, 'ț': 0, 'b':0, 'c':0, 'd': 0, 'e': 0, 'f':0, 'g':0, "h":0, 'i':0, 'j':0, 'k':0, 'l':0,
                 'm':0, 'n':0, 'o':0, 'p':0, 'q':0, 'r':0, 's':0, 't':0, 'u':0, 'v':0, 'w':0, 'x':0, 'y':0, 'z':0}
-    complete_words = []
-    
-    # Get a list with only the complete words
-    for word in words:
-        # Get rid of the newline character at the end of the line
-        new = word.strip("\n")
-        word = new.split(";")
-        complete_words.append(word[2])
     
     # For every word, count the appearace of every letter
-    for word in complete_words:
+    for word in words:
+        new = word.strip("\n")
+        word = new.split(";")
         for ch in alphabet:
-            as_list = list(word)
+            as_list = list(word[2])
             result[ch] += as_list.count(ch.upper())
     
     sorted_by_values = dict(sorted(result.items(), key=lambda item: item[1], reverse=True))
